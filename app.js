ceec3bcdb0545cd9798e92ea4692453ba12bb7b4
Roger Villars <roger.villars@bluewin.ch>
2014-07-26 15:03:52 +0000

    corrected manifest.yml for cloudfoundry

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ if ('production' == env) {
     app.use(morgan('default'));
     var mongodb = require('mongodb');
     var vcap = JSON.parse(process.env.VCAP_SERVICES);
+    console.log(JSON.stringify(vcap));
     mongoUrl = vcap['mongolab-n/a'][0].credentials.uri;
     port = process.env.VCAP_APP_PORT;
 };
